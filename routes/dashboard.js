f44a61dcb0a1b09cf1462fde690171d1489ddd4f
ryp <rateyourpedal@gmail.com>
2020-12-01 17:24:48 -0300

    formulario envia

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -7,4 +7,13 @@ router.get('/dashboard', (req, res) => {
     res.render('profile/dashboard');
 });
 
+
+router.get('/dashboard/add', (req, res) => {
+    res.render('profile/add');
+});
+
+router.post('/dashboard/add', (req, res) => {
+    console.log(req.body);
+    res.send('recibido');
+});
 module.exports = router;
