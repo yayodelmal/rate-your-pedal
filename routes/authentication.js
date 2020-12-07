a53eaaa6d2116d5398423c4a16df0789e1f84b06
ryp <rateyourpedal@gmail.com>
2020-12-07 15:31:04 -0300

    mostrar y ocultar contaseña inicio de registro con passport

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,13 +1,21 @@
 let express = require('express');
 let router = express.Router();
 
-
+const passport = require('passport');
 
 const pool = require('../db');
-
+/* 
 router.post('/signup', (req, res) => {
-    console.log(req.body);
-    res.send('ok');
-});
+        passport.authenticate('local.signup', {
+            successRedirect: '/dashboard',
+            failureRedirect: '/',
+            failureFlash: true
+        }))
+*/
+router.post('/signup', passport.authenticate('local.signup', {
+    successRedirect: '/dashboard',
+    failureRedirect: '/',
+    failureFlash: true
+}));
 
 module.exports = router;
